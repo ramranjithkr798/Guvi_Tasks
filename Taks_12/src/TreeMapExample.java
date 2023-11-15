cf9410ba4fb1cf40f423107040d95787460489b0
ranjith <ramranjithkr798@gmail.com>
2023-11-15 16:18:01 +0530

    Second Commit

--- Taks_12/src/TreeMapExample.java
+++ Taks_12/src/TreeMapExample.java
@@ -1,4 +1,8 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class TreeMapExample {
@@ -11,7 +15,7 @@ public class TreeMapExample {
 		boolean flage=true;
 		while(flage) {
 			System.out.println("Enter the option which need to perform in the Tree Map \n1. Add in data in Map"
-					+ "\n2. Delete in data in Map \n3. Delete All element in the Map \n4. Display the Tree Map \n"
+					+ "\n2. Delete in data in Map \n3. Delete All element in the Map \n4. Display the data in Tree Map as a sorted manner\n"
 					+ "5. Exit from the Program");
 			choice=scan.nextInt();
 			scan.nextLine();
@@ -30,7 +34,20 @@ public class TreeMapExample {
 				tm.clear();
 				break;
 			case 4:
-				//need to add the code for the display the based on the name (asseding order)
+				Map sortedMap = valueSort(tm);
+				 Set set = sortedMap.entrySet(); 
+				  
+			        // Get an iterator 
+			        Iterator i = set.iterator(); 
+			  
+			        while (i.hasNext()) 
+			        { 
+			            Map.Entry mp = (Map.Entry)i.next(); 
+			  
+			            System.out.print(mp.getKey() + ": "); 
+			  
+			            System.out.println(mp.getValue()); 
+			        } 
 				break;
 			case 5:
 				System.out.println("Thanks you ...!!!");
@@ -43,5 +60,31 @@ public class TreeMapExample {
 			}
 		}
 	}
+	
+	public static <K, V extends Comparable<V> > Map<K, V> 
+    valueSort(final Map<K, V> map) {
+		Comparator<K> valueComparator = new Comparator<K>() 
+        { 
+              
+            public int compare(K k1, K k2) 
+            { 
+  
+                int comp = map.get(k1).compareTo(map.get(k2)); 
+  
+                if (comp == 0) 
+                     return 1; 
+  
+                else
+                     return comp; 
+            } 
+        }; 
+  
+        // SortedMap created using the comparator 
+        Map<K, V> sorted = new TreeMap<K, V>(valueComparator); 
+  
+        sorted.putAll(map); 
+  
+        return sorted; 
+	} 
 
 }//C:\Users\ramra\git\Guvi_Tasks\.git
